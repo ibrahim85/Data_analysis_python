e283b5f7e9e27534b6494870f0dd685fb42c4fef
majubun <majubunorbert@gmail.com>
2016-01-14 15:45:52 -0500

    Segmenting under construction

--- testing.py
+++ testing.py
@@ -4,9 +4,12 @@ import physics
 import math
 import matplotlib.pyplot as plt
 import baseMetrics as b
+from patient import patient
 
 #print(stat.distributionSorter([3,6,8,9,1,3,54,6,2,56,2,3,1,55,77,32,2,4,6],[1,2,3,4,5,6,7,8,9,10]))
 
+
+"""
 y = []
 x = []
 for i in range(600):
@@ -25,3 +28,9 @@ plt.plot(x[0:len(x)-1],(z),color='g')
 plt.plot(x,[0] * len(x),color='y')
 plt.plot([math.pi/2] * len(z),z,color='y')
 plt.show()
+
+"""
+#def __init__(self, name, x, y, time, distance, events):
+peti = patient('peti',0,0,0,0,[['success',1],['fail',2],['nnyenye',3]])
+
+print(peti.Events[1][1])
