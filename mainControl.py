93eb45b49f00ca01127dc96243b98d0b03de7805
majubun <majubunorbert@gmail.com>
2016-01-18 13:26:56 -0500

    Introduce early-late differentiation

--- mainControl.py
+++ mainControl.py
@@ -26,31 +26,35 @@ for i in range(0, len(patients)):
 
 
     patientSpeedSample = [] #[0] * speedResolution
-    patientSpeedDistance = [] #[0] * spaceResolution
+    patientSpeedDistance = [[],[],[]] #[0] * spaceResolution
     patientGradientSample = [] #[0] * gradientResolution
     patientGradientDistance = [] #[0] * spaceResolution
+    plots = []
 
-    for j in range(1, sessions_nums[0][i]+1):
-        patient_address =base_address + patients[i] + '\A' + str(math.floor(j / 10)) + str(j % 10)
-        os.chdir(patient_address)
+    for time in range(3):
+        for j in range(math.floor(time/3 * sessions_nums[0][i]) + 1, math.ceil((time+1) / 3 * sessions_nums[0][i]) + 1):
+            patient_address =base_address + patients[i] + '\A' + str(math.floor(j / 10)) + str(j % 10)
+            os.chdir(patient_address)
 
-        Patient = readin.patientData(patients, i, j)
+            Patient = readin.patientData(patients, i, j)
 
-        #for x in range(len(Patient.Events[1][0])):
-        #    print(Patient.Events[1][0][x][0][0][0])
+            #for x in range(len(Patient.Events[1][0])):
+            #    print(Patient.Events[1][0][x][0][0][0])
 
-        patientSpeedSample.append(SpeedSample.mainFunction(Patient, 1, referenceSpaces.speed))
-        patientSpeedDistance.append(SpeedDistance.mainFunction(Patient, 1, referenceSpaces.space, 1))
-        #patientGradientSample.append(GradientSample.mainFunction(Patient.name, Patient.X, Patient.Y, Patient.T))
-        patientGradientDistance.append(GradientDistance.mainFunction(Patient, 1, referenceSpaces.space)) #(patient_data, step, grid)
+            time_map = {0: 'Early-training', 1: 'Mid-training', 2: 'Late-training'}
 
-    display.result(referenceSpaces.space, baseStatistics.mean(patientSpeedDistance, 'vertical'), baseStatistics.std(patientSpeedDistance, 'vertical'), Patient.name + ' Speed = F(distance)' )
-    display.result(physics.scale(referenceSpaces.speed, (1 / screen_to_cm_ratio)), baseStatistics.mean(patientSpeedSample, 'vertical'), baseStatistics.std(patientSpeedSample, 'vertical'), Patient.name + ' # = F(speed)' )
-    display.result(referenceSpaces.space, baseStatistics.mean(patientGradientDistance, 'vertical'), baseStatistics.std(patientGradientDistance, 'vertical'), Patient.name + 'Gradient = F(distance)')
+            #patientSpeedSample.append(SpeedSample.mainFunction(Patient, 1, referenceSpaces.speed))
+            patientSpeedDistance[time].append(SpeedDistance.mainFunction(Patient, 1, referenceSpaces.space, 1))
+            #patientGradientSample.append(GradientSample.mainFunction(Patient.name, Patient.X, Patient.Y, Patient.T))
+            #patientGradientDistance.append(GradientDistance.mainFunction(Patient, 1, referenceSpaces.space)) #(patient_data, step, grid)
 
-    plt.draw()
+        #appending plots as returning values is only necessary for the LEGEND. If it comes out to be timeconsuming should be ommitted and resolved.
+        plots.append(display.result(referenceSpaces.space, baseStatistics.mean(patientSpeedDistance[time], 'vertical'), baseStatistics.std(patientSpeedDistance[time], 'vertical'), i,  Patient.name + ' Speed = F(distance)', 1, timing=time_map[time]))
+        #display.result(physics.scale(referenceSpaces.speed, (1 / screen_to_cm_ratio)), baseStatistics.mean(patientSpeedSample, 'vertical'), baseStatistics.std(patientSpeedSample, 'vertical'), i,  Patient.name + ' # = F(speed)', 0)
+        #display.result(referenceSpaces.space, baseStatistics.mean(patientGradientDistance, 'vertical'), baseStatistics.std(patientGradientDistance, 'vertical'), i, Patient.name + 'Gradient = F(distance)', 3)
 
-
+        plt.draw()
+    plt.legend(handles=plots)
 plt.show()
 
 
